f0603ab154119bf8f60457fbcbabb8a7cafb605e
david78k <root@c11node28.(none)>
2014-01-15 09:01:03 -0500

    basic.m

--- basic.m
+++ basic.m
@@ -228,6 +228,7 @@ x = ['PRECOPY', 'POSTCOPY', 'HYBRID', 'PRO-HYBRID'];
 %plot(x, A(:,2)/1000000, x, A(:,3)/1000000, '-.*');
 %plot(A(:,2));
 bar(A(:,1))
+set(gca, 'COPY METHOD')
 xlabel('COPY METHOD', FontSize, '14');
 ylabel('TIME');
 %legend('PRECOPY', 'POSTCOPY', 'HYBRID', 'PRO-HYBRID');
