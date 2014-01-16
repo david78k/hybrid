28151f7e00c0929af08a7ea3f455bf1648cc7bd2
david78k <root@c11node28.(none)>
2014-01-16 13:34:58 -0500

    basic.m

--- basic.m
+++ basic.m
@@ -277,7 +277,7 @@ x = {'PRE', 'POST', 'HYBR', 'PROH100', 'PROH75', 'PROH50', 'PROH25', 'PROH0'};
 
 %xtics = 1:4:16
 %bar(xtics, A(:,1))
-bar(A(:,1))
+bar(A(:,1), 'Width', 2)
 set(gca, 'XTick', 1:2:8, 'XTickLabel', x, 'FontSize', fontsize)
 xlabel('COPY METHOD', 'FontSize', fontsize);
 ylabel('TOTAL TIME (SEC)', 'FontSize', fontsize);
