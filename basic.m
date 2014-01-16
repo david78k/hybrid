3bdc1585603f4797a8b570a04780ecc3a99be8ba
david78k <root@c11node28.(none)>
2014-01-16 14:05:43 -0500

    basic.m

--- basic.m
+++ basic.m
@@ -274,7 +274,8 @@ x = {'PRE', 'POST', 'HYBR', 'PROH100', 'PROH75', 'PROH50', 'PROH25', 'PROH0'};
 %x = {'1', '2', '3', '4', '5', '6', '7', '8'};
 xinterval = 3
 %xintervals = 1:xinterval:(xinterval*length(x));
-xintervals = -2:3:3*length(x);
+%xintervals = 1:3:3*length(x);
+xintervals = 1:length(x);
 %plot(x, A(:,2)/1000000, x, A(:,3)/1000000, '-.*');
 %plot(A(:,2));
 
@@ -282,7 +283,7 @@ xintervals = -2:3:3*length(x);
 %bar(xtics, A(:,1))
 %bar(A(:,1), XTicksLabelStyle = Diagonal)
 bar(xintervals, A(:,1))
-set(gca, 'XTick', 1:3:24, 'XTickLabel', x, 'FontSize', fontsize)
+set(gca, 'XTick', 1:8, 'XTickLabel', x, 'FontSize', fontsize)
 %set(gca, 'XTick', xintervals, 'XTickLabel', x, 'FontSize', fontsize)
 xlabel('COPY METHOD', 'FontSize', fontsize);
 ylabel('TOTAL TIME (SEC)', 'FontSize', fontsize);
