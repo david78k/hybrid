8f510f32f7c2f75436ace881a0bee9fee47abe30
david78k <root@c11node28.(none)>
2014-01-16 10:04:00 -0500

    basic.m

--- basic.m
+++ basic.m
@@ -4,11 +4,11 @@ r = 4096
 r *= 1024 % convert to KB
 
 % page dirty rate in percent (%): against to total memory pages
-readrate = 0
+readrate = 100
 prefix = strcat ("M4G_R", num2str(readrate))
 readrate /= 100
 
-dirtyrate = 100 
+dirtyrate = 0 
 prefix = strcat (prefix, "_W", num2str(dirtyrate))
 dirtyrate /= 100
 
