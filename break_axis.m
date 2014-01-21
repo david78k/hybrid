15c223235620ae759241b1f17e7cce6f1b56d5c4
david78k <root@c11node28.(none)>
2014-01-21 10:58:06 -0500

    break_axis.m

--- break_axis.m
+++ break_axis.m
@@ -28,11 +28,10 @@ delete(gcf);
 a2=axes('position',[p0(1) p0(4)/2+.145 p0(3) p0(4)/2], 'ylim', ylim2)
 %a2=axes('position',[p0(1) p0(2) p0(3) p0(4)/2]);
 %barh(a);
-%bar(a);
-bar(a2);
+bar(a);
 
 %set(a2,'ylim',ylim1);
-%set(a2,'ylim',ylim2);
+set(a2,'ylim',ylim2);
 %set(a2,'xlim',xlim,'ylim',ylim2);
 %set(a2,'xtick',[]);
 %set(a2,'xcolor',get(a2,'color'));
