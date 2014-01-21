211728b255167653d01384bf43571a59864ddf84
david78k <root@c11node28.(none)>
2014-01-21 03:24:54 -0500

    break_axis.m

--- break_axis.m
+++ break_axis.m
@@ -12,20 +12,24 @@ delete(gcf);
 % create axis1
 %a1=axes('position',[p0(1) p0(2) p0(3)/2-.025 p0(4)]);
 a1=axes('position',[p0(1) p0(2) p0(3) p0(4)/2]);
+a2=axes('position',[p0(1) p0(4)/2+.145 p0(3) p0(4)/2])
 %barh(a);
 bar(a);
-set(a1,'ylim',ylim1);
+%set(a1,'ylim',ylim1);
+set(a2,'xlim',xlim,'ylim',ylim2);
+set(a2,'xtick',[]);
 %box off;
 
 % create axis2
 %a2=axes('position',[p0(1)+p0(3)/2+.025 p0(2) p0(3)/2-.025 p0(4)]);
 %a2=axes('position',[p0(1) p0(4)/2+.145 p0(3) p0(4)/2])
-a2=axes('position',[p0(1) p0(4)/2+.145 p0(3) p0(4)/2])
+%a2=axes('position',[p0(1) p0(4)/2+.145 p0(3) p0(4)/2])
 %barh(a);
 bar(a);
 
-set(a2,'xlim',xlim,'ylim',ylim2);
-set(a2,'xtick',[]);
+set(a1,'ylim',ylim1);
+%set(a2,'xlim',xlim,'ylim',ylim2);
+%set(a2,'xtick',[]);
 %set(a2,'xcolor',get(a2,'color'));
 %box off
 
