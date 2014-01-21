4ddb3a5b0fef4e02d6b3cf12a2bc0b1293ccb8aa
david78k <root@c11node28.(none)>
2014-01-21 03:22:34 -0500

    break_axis.m

--- break_axis.m
+++ break_axis.m
@@ -15,7 +15,7 @@ a1=axes('position',[p0(1) p0(2) p0(3) p0(4)/2]);
 %barh(a);
 bar(a);
 set(a1,'ylim',ylim1);
-box off;
+%box off;
 
 % create axis2
 %a2=axes('position',[p0(1)+p0(3)/2+.025 p0(2) p0(3)/2-.025 p0(4)]);
