a0e766a83ba2fe7fc7068f54861007a4ea361576
david78k <root@c11node28.(none)>
2014-01-21 09:45:04 -0500

    break_axis.m

--- break_axis.m
+++ break_axis.m
@@ -16,7 +16,7 @@ delete(gcf);
 a1=axes('position',[p0(1) p0(2) p0(3) p0(4)/2]);
 %a2=axes('position',[p0(1) p0(4)/2+.145 p0(3) p0(4)/2])
 %barh(a);
-%bar(a);
+bar(a);
 set(a1,'ylim',ylim1);
 %set(a2,'xlim',xlim,'ylim',ylim2);
 %set(a2,'xtick',[]);
