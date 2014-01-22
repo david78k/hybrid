4bb2ea10489db36a29a59cfdba31ce0a3f34bac9
david78k <root@c11node28.(none)>
2014-01-22 01:23:50 -0500

    basic_break.m

--- basic_break.m
+++ basic_break.m
@@ -306,9 +306,9 @@ set (a1, 'ylim', ylim1);
 %xlabel('COPY METHOD', 'FontSize', fontsize);
 %ylabel('DOWNTIME (SEC)', 'FontSize', fontsize);
 
-%a2 = axes ('position', [p0(1) p0(4)/2+.15 p0(3) p0(4)/2]);
-%bar(xintervals, A(:,2))
-%set(a2, 'ylim', ylim2, 'FontSize', fontsize);
+a2 = axes ('position', [p0(1) p0(4)/2+.15 p0(3) p0(4)/2]);
+bar(xintervals, A(:,2))
+set(a2, 'ylim', ylim2, 'FontSize', fontsize);
 %set(a2, 'xtick', []);
 %ylabel('DOWNTIME (SEC)', 'FontSize', fontsize);
 
