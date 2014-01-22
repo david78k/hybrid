ec538354b4c1ccf38b9308e6b16af04ad782eda9
david78k <root@c11node28.(none)>
2014-01-22 01:31:00 -0500

    basic_break.m

--- basic_break.m
+++ basic_break.m
@@ -296,7 +296,8 @@ delete(gcf);
 
 %cla;
 
-a1 = axes ('position', [p0(1) p0(2) p0(3) p0(4)/2]);
+%a1 = axes ('position', [p0(1) p0(2) p0(3) p0(4)/2]);
+a1 = axes ('position', [p0(1) p0(2) p0(3) p0(4)*2/3]);
 
 bar(xintervals, A(:,2))
 %set(gca, 'XTick', xintervals, 'XTickLabel', x, 'FontSize', fontsize)
@@ -306,7 +307,7 @@ set (a1, 'ylim', ylim1);
 xlabel('COPY METHOD', 'FontSize', fontsize);
 %ylabel('DOWNTIME (SEC)', 'FontSize', fontsize);
 
-a2 = axes ('position', [p0(1) p0(4)/2+.15 p0(3) p0(4)/3]);
+a2 = axes ('position', [p0(1) p0(4)*2/3+.15 p0(3) p0(4)/3]);
 bar(xintervals, A(:,2))
 set(a2, 'ylim', ylim2, 'FontSize', fontsize);
 set(a2, 'xtick', []);
